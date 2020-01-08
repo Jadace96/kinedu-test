11ff834bcd25a9864fb0f162eb510c83bbac5ab7
Jadace96 <jadace96@gmail.com>
2020-01-08 00:00:34 -0500

    Added css-loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "css-loader" }]
       }
     ]
   },
