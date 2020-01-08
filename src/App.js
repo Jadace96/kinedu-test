1b93f71fdad4b574aa2ff4eb7c7358cb076927d1
Jadace96 <jadace96@gmail.com>
2020-01-08 01:11:59 -0500

    Added styled-components config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+
+import theme from "./themes";
+
+const AppWrapper = styled.div.attrs(() => ({
+  id: "AppWrapper"
+}))`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
 
 function App() {
   return (
-    <div>
-      <h2>Kinedu test!!!</h2>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppWrapper>
+        <h2>Kinedu test!!!</h2>
+      </AppWrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/colors.js
@@ -0,0 +1,3 @@
+const AppColors = {};
+
+export { AppColors as colors };
