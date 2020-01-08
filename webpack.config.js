2eff35c2acb19b3ab5f27a65f513abd01a39f967
Jadace96 <jadace96@gmail.com>
2020-01-07 23:31:35 -0500

    Added initial configuration with webpack, babel and react

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // ENTRY POINT
+  entry: path.resolve(__dirname, "src/index.js"),
+
+  // OUTPUT DIRECTORY
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle-[hash].js"
+  },
+
+  // EVIROMENT MODE
+  mode: process.env.NODE_ENV || "development",
+
+  // PATH RESOLVE
+  resolve: {
+    extensions: [".js", ".json", ".jsx"],
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  },
+
+  // LOADERS
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  // PLUGINS
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html"
+    })
+  ],
+
+  // DEV SERVER ENTRY POINT
+  devServer: {
+    port: 3000,
+    open: true,
+    contentBase: path.resolve(__dirname, "src")
+  }
+};
